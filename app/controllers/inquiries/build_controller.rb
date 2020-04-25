cd39fd73c3751d63574ab8ee66a74cc36b133fc6
lewis nyoike <nyoikelewis@gmail.com>
2020-04-25 22:57:43 +0300

    For the wicked steps

---
+++ app/controllers/inquiries/build_controller.rb
@@ -0,0 +1,21 @@
+class Inquiries::BuildController < ApplicationController
+  include Wicked::Wizard
+
+  steps :contact_information, :store_information, :billing_information, :final_step
+
+  def show
+    @inquiry = Inquiry.find(params[:inquiry_id])
+    render_wizard
+  end
+  
+  def update
+    @inquiry = Inquiry.find(params[:inquiry_id])
+    @inquiry.update_attributes(inquiry_params)
+    render_wizard @inquiry
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:full_name, :phone_number, :email, :store_name, :domain_name, :preffered_name, :plan, :billing_type, :web_administration, :message)
+  end
+end
