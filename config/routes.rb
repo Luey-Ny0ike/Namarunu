0e2b2f7665abd7bf063af1b2a6a398b9e9d59f33
lewis nyoike <nyoikelewis@gmail.com>
2020-04-25 22:59:34 +0300

    This is unnessesarily confusing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :inquiries
+  resources :inquiries do
+    resources :build, controller:'inquiries/build'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 # STATIC PAGES ROUTES
