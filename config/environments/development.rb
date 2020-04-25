bafd4e01796f5d8ae6acb5c99d16d94d5de316d2
lewis nyoike <nyoikelewis@gmail.com>
2020-04-26 02:27:50 +0300

    To test the mailer on develpment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,19 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:            Rails.application.credentials.sendgrid_username,
+    password:             Rails.application.credentials.sendgrid_password,
+    domain:               'localhost',
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
