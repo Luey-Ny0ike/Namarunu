6757b719f004db98cdeb7708ec72da9bbc5571ec
lewis nyoike <nyoikelewis@gmail.com>
2020-09-11 23:10:55 +0300

    More routes for the extra pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
 # STATIC PAGES ROUTES
 root 'static_pages#index'
+get 'products', to: 'static_pages#products', as: 'products'
 get '/products/namarunu-store', to: 'static_pages#store-sell', as: 'store_sell'
+get 'products/web-administration-services', to: 'static_pages#web-administration', as: 'web_administration'
+get 'products/startup-package', to: 'static_pages#startup-package', as: 'startup_package'
 get '/pricing', to:'static_pages#pricing', as: 'pricing'
 end
