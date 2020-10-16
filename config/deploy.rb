c99c50f746e1dbc6e501d1714dbaeb2ac781c396
lewis nyoike <nyoikelewis@gmail.com>
2020-10-16 23:32:54 +0300

    To minimize storage usage

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :linked_files, %w{config/master.key}
 # set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
-# set :keep_releases, 5
+set :keep_releases, 2
 
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
