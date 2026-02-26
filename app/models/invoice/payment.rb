1849b5f139b8b81981ea05c8454045739816a2ac
GithinjiV <githinjigicheru146@gmail.com>
2026-02-26 22:28:40 +0300

    Use default rails testing framework

--- app/models/invoice/payment.rb
+++ app/models/invoice/payment.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
-module Invoice
-  class Payment < ApplicationRecord
-    STATUSES  = %w[pending succeeded failed refunded].freeze
-    PROVIDERS = %w[mpesa equity_bank flutterwave].freeze
+class Invoice::Payment < ApplicationRecord
+  STATUSES  = %w[ pending succeeded failed refunded ].freeze
+  PROVIDERS = %w[ mpesa equity_bank paystack ].freeze
 
-    belongs_to :invoice
+  belongs_to :invoice
 
-    validates :provider,
-              presence: true,
-              inclusion: { in: PROVIDERS }
+  validates :provider,
+    presence: true,
+    inclusion: { in: PROVIDERS }
 
-    validates :status,
-              presence: true,
-              inclusion: { in: STATUSES }
+  validates :status,
+    presence: true,
+    inclusion: { in: STATUSES }
 
-    validates :amount_cents,
-              numericality: { only_integer: true, greater_than: 0 }
+  validates :amount_cents,
+    numericality: { only_integer: true, greater_than: 0 }
 
-    validates :currency,
-              presence: true,
-              inclusion: { in: Store::CURRENCIES }
-  end
+  validates :currency,
+    presence: true,
+    inclusion: { in: Store::CURRENCIES }
 end
