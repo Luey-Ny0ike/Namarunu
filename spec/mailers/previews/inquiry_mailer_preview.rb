99c7fb292e8b0bd5ccfa4c431b2abce5e9ca0c82
GithinjiV <githinjigicheru146@gmail.com>
2026-02-26 22:28:40 +0300

    Use default rails testing framework

--- spec/mailers/previews/inquiry_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/inquiry_mailer
-class InquiryMailerPreview < ActionMailer::Preview
-  def new_inquiry_email
-    @inquiry = Inquiry.first
-    InquiryMailer.with(inquiry: @inquiry).new_inquiry_email
-  end
-end
